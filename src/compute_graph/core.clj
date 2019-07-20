9077af945bd5479b84d45742d8af689a6e400c9a
Valentin Waeselynck <vwaeselynck@ap2s.fr>
2019-07-20 12:05:26 +0200

    Solution 1: naive recursive implementation.

--- src/compute_graph/core.clj
+++ src/compute_graph/core.clj
@@ -2,4 +2,20 @@
 
 (defn compute
   [graph input-map output-keys]
-  (comment FIXME))
+  (letfn [(aux [k]
+            (if (contains? input-map k)
+              (get input-map k)
+              (if-some [step (get graph k)]
+                (apply (:fn step)
+                  (mapv aux
+                    (:deps step)))
+                (throw
+                  (ex-info
+                    (str "Key missing from graph or input: " (pr-str k))
+                    {:key k
+                     :graph graph})))))]
+    (reduce
+      (fn [m k]
+        (assoc m k (aux k)))
+      {}
+      output-keys)))
