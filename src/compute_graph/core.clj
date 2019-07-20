5649a5e69d1dfdec8d54c35107f075acc7091b63
Valentin Waeselynck <vwaeselynck@ap2s.fr>
2019-07-20 12:08:15 +0200

    Solution 2: mutable cache.

--- src/compute_graph/core.clj
+++ src/compute_graph/core.clj
@@ -2,20 +2,27 @@
 
 (defn compute
   [graph input-map output-keys]
-  (letfn [(aux [k]
-            (if (contains? input-map k)
-              (get input-map k)
-              (if-some [step (get graph k)]
-                (apply (:fn step)
-                  (mapv aux
-                    (:deps step)))
-                (throw
-                  (ex-info
-                    (str "Key missing from graph or input: " (pr-str k))
-                    {:key k
-                     :graph graph})))))]
-    (reduce
-      (fn [m k]
-        (assoc m k (aux k)))
-      {}
-      output-keys)))
+  (let [cache-atom (atom {})]
+    (letfn [(aux [k]
+              (let [cache @cache-atom]
+                (if (contains? cache k)
+                  (get cache k)
+                  (let [ret
+                        (if (contains? input-map k)
+                          (get input-map k)
+                          (if-some [step (get graph k)]
+                            (apply (:fn step)
+                              (mapv aux
+                                (:deps step)))
+                            (throw
+                              (ex-info
+                                (str "Key missing from graph or input: " (pr-str k))
+                                {:key k
+                                 :graph graph}))))]
+                    (swap! cache-atom assoc k ret)
+                    ret))))]
+      (reduce
+        (fn [m k]
+          (assoc m k (aux k)))
+        {}
+        output-keys))))
