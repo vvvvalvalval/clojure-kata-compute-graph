a84ca7db90f5303619aa523a809238823de630d3
Valentin Waeselynck <vwaeselynck@ap2s.fr>
2019-07-20 12:17:24 +0200

    Solution 3: functional cacheing using an accumulator.

--- src/compute_graph/core.clj
+++ src/compute_graph/core.clj
@@ -2,27 +2,24 @@
 
 (defn compute
   [graph input-map output-keys]
-  (let [cache-atom (atom {})]
-    (letfn [(aux [k]
-              (let [cache @cache-atom]
-                (if (contains? cache k)
-                  (get cache k)
-                  (let [ret
-                        (if (contains? input-map k)
-                          (get input-map k)
-                          (if-some [step (get graph k)]
-                            (apply (:fn step)
-                              (mapv aux
-                                (:deps step)))
-                            (throw
-                              (ex-info
-                                (str "Key missing from graph or input: " (pr-str k))
-                                {:key k
-                                 :graph graph}))))]
-                    (swap! cache-atom assoc k ret)
-                    ret))))]
-      (reduce
-        (fn [m k]
-          (assoc m k (aux k)))
-        {}
-        output-keys))))
+  (letfn [(ensure-key-in-acc [acc k]
+            (if (contains? acc k)
+              acc
+              (if (contains? input-map k)
+                (assoc acc
+                  k
+                  (get input-map k))
+                (if-some [step (get graph k)]
+                  (let [deps-ks (:deps step)
+                        acc-with-deps (reduce ensure-key-in-acc acc deps-ks)
+                        deps-values (mapv acc-with-deps deps-ks)
+                        v (apply (:fn step) deps-values)]
+                    (assoc acc-with-deps k v))
+                  (throw
+                    (ex-info
+                      (str "Key missing from graph or input: " (pr-str k))
+                      {:key k
+                       :graph graph}))))))]
+    (select-keys
+      (reduce ensure-key-in-acc {} output-keys)
+      output-keys)))
